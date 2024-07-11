f62fb61d5a53e4e257637e3549c82da327030aad
manasaj123 <manasajangili99@gmail.com>
2024-07-11 16:32:00 +0530

    successfully added

---
+++ number.java
@@ -0,0 +1,12 @@
+public class Client {
+
+	public static void main(String[] args) {
+          int n1=10;
+          int n2=20;
+          int n3=n1+n2;
+          
+ 	 system.out.println("addition of two numbers="+n3)
+		
+	}
+
+}
